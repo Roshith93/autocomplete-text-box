d90dc2e724cc67265a49f9658255abbae2371aa4
roshith93 <r.roshith@gmail.com>
2019-06-18 15:07:16 +0530

    initial blood

---
+++ src/component/HideableText.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class HiddenText extends  Component {
+    render(){
+        return(
+            <div>
+                <button>Hello </button>
+            </div>
+        )
+    }
+}
+export default HiddenText;
